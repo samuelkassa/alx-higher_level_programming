66bd2dac1d97e17593273025c6a872c90508528a
samuelkassa <samuelkassahun22@gmail.com>
2023-03-09 16:34:07 -0800

    Update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ = "__main__":
     couner = len(argv)
     index = 1
     if counter == 0:
-        print("{:d} arguments.").format(counter))
+        print("{:d} arguments.".format(counter))
     elif counter == 1:
         print("{:d} argument:".format(conter))
         print("{:d} {:s}".format(index, sys.argv[1]))
