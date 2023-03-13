8882fac722286524b93d25993fcc7f6f5ea016ba
samuelkassa <samuelkassahun22@gmail.com>
2023-03-13 11:19:26 -0700

    shallow copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (idx < 0) or (idx >= len(my_list)):
+        return(my_list)
+    else:
+        list_copy = [x for x in my_list]
+        list_copy[idx] = element
+        return(list_copy)
