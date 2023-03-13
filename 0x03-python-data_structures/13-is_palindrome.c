be37da9c726bdf77a6eb12bf3ba2953e3ef883e7
samuelkassa <samuelkassahun22@gmail.com>
2023-03-13 12:30:53 -0700

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,6 +10,7 @@ int is_palindrome(listint_t **head);
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *node = *head, *next, *prev = NULL;
+
 	while (node)
 	{
 		next = node->next;
@@ -32,6 +33,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *tmp, *rev, *mid;
 	size_t size = 0, i;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	tmp = *head;
