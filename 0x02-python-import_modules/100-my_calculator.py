4b97974d6fc45b362f0b70a45fbed4196f071847
samuelkassa <samuelkassahun22@gmail.com>
2023-03-13 07:50:10 -0700

    Advanced problem

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,16 +9,18 @@ if __name__ == '__main__':
     if (argc - 1) < 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    elif argv[2] not in operators:
+    elif argv[2] not in vvioperators:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
     else:
         a, b = int(argv[1]), int(argv[3])
         if argv[2] == '+':
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif argv[2] == '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, add(a, b)))
-        elif argv[2] == '*':
-            print('{:d} * {:d} = {:d}'.format(a, b, add(a, b)))
-        else:
-            print('{:d} / {:d} = {:d}'.format(a, b, add(a, b)))
+            if argv[2] == '+':
+                print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+            elif argv[2] == '-':
+                print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+            elif argv[2] == '*':
+                print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+            else:
+                print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+                            
