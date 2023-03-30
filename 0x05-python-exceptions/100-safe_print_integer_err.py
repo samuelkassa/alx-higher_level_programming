12cba9bfbdc59e4ac15986a1f726fa87497afe4c
samuelkassa <samuelkassahun22@gmail.com>
2023-03-30 16:27:34 -0700

    printing an integer with the exception being printed on sterr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,11 @@
 from __future__ import print_function
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
+    except Exception as e:
         print("Exception {}".format(e), file=sys.stderr)
         return False
