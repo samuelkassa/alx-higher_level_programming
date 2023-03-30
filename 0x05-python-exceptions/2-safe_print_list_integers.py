5c3baad20d82e49c701dc896caf26ba2ff20a179
samuelkassa <samuelkassahun22@gmail.com>
2023-03-30 14:39:30 -0700

    integer printing

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i, j = 0, 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            j += 1
+        except (ValueError, IndexError):
+            pass
+        i += 1
+    print()
+    return j
