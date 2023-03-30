cbbb631339652bb504e49deeb5238566f2f3de11
samuelkassa <samuelkassahun22@gmail.com>
2023-03-30 15:35:11 -0700

    dividing a list by another list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             new_list[i] = 0
         finally:
-            i++
+            i += 1
     return new_list
