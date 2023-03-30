9c2594e05e53fb886358c9d716658ae7c29e7d3a
samuelkassa <samuelkassahun22@gmail.com>
2023-03-30 14:18:19 -0700

    checking if the value is integer with try except

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,8 @@
 
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-    except ValueError:
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
         return False
+    else:
+        return True
