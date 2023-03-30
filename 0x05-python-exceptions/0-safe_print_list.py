169426e3744e0410aa7be901113f1bafa6b42f99
samuelkassa <samuelkassahun22@gmail.com>
2023-03-30 13:26:33 -0700

    printing list elements

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    try:
+        while n is not x:
+            print(my_list[n])
+            n += 1
+    except IndexError:
+        None
+    print()
+    return n
