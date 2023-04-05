1b8004e9b58f3afec7ff82ce9b4347854a5e8272
samuelkassa <samuelkassahun22@gmail.com>
2023-04-04 20:49:50 -0700

    initialzing the square class with private attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python class for a square with private instance fields"""
+
+
+class Square:
+    """Python class for instantiate square"""
+
+    def __init__(self, size):
+        """Initialize the class
+        Args
+         size(int): the size of the square
+         """
+         self.__size = size
