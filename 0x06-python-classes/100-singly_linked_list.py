8df7cd341703a06b314db4dfe067288f1670383c
samuelkassa <samuelkassahun22@gmail.com>
2023-04-06 09:47:57 -0700

    Singly linked list in oop

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -69,11 +69,11 @@ class SinglyLinkedList:
             new.next_node = tmp.next_node
             tmp.next_node = new
 
-     def __str__(self):
-         """Define the print() representation of a SinglyLinkedList."""
-         values = []
-         tmp = self.__head
-         while tmp is not None:
-             values.append(str(tmp.data))
-             tmp = tmp.next_node
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
         return ('\n'.join(values))
