6c27acb89fe1038d2636c422367cfe90de31b97c
samuelkassa <samuelkassahun22@gmail.com>
2023-04-07 11:15:03 -0700

    printing first and last name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,7 @@ def say_my_name(first_name, last_name):
         TypeError: if first and last name are not strings
     """
     if not (isinstance(first_name, str)):
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError("first_name must be a string")
     if not (isinstance(last_name, str)):
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
