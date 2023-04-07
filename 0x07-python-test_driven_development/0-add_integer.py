df58cc936dc5d93576c8163f5fc0c56422e44295
samuelkassa <samuelkassahun22@gmail.com>
2023-04-07 07:14:56 -0700

    doctest and integer addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines integer addition function"""
+
+
+def add_integer(a, b=98):
+    """Return the integer addition of a and b
+
+    Float arguments are typecasted to ints before addition is perfomed
+
+    Raises:
+        TypeError: if either a or b is non-integer or non-float
+    """
+    if ((not isinstance(a, int)) and (not isinstance(a, float))):
+        raise TypeError("a must an integer")
+    if ((not isinstance(b, int)) and (not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    else:
+        return (int(a) + int(b))
