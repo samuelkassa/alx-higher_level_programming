a62ad295f00b88586bd86bdcae6446a07b081399
= <=>
2023-08-23 18:51:50 -0700

    Making some adjutment

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Script prints all City objects
-Takes three arguments
-    mysql username
-    mysql password
-    database name
-Connects to host localhost and default port (3306)
+""" Start a link class to table in database
 """
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 if __name__ == "__main__":
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import aliased, sessionmaker
-    from model_state import Base, State
-    from model_city import City
-    from sys import argv
-    Session = sessionmaker()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    session = Session(bind=engine)
-    Base.metadata.create_all(engine)
-    result = (session.query(State.name, City.id, City.name).filter(
-        State.id == City.state_id).order_by(City.id).all())
-    for i in result:
-        print("{}: ({:d}) {}".format(i[0], i[1], i[2]))
-    session.close()
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    State.cities = relationship("City",
+                                order_by=City.id, back_populates="state")
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    eng = create_engine(connection.format(user_name, password, db_name),
+                        pool_pre_ping=True)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    query = session.query(State, City).\
+        filter(City.state_id == State.id).all()
+    for row in query:
+        print("{}: ({}) {}".format(row[0].name, row[1].id, row[1].name))
